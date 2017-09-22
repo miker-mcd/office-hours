a8fb0635efedd896c14f9bc7337123672af1abe7
David Keller <email": null>
2017-09-22 10:01:11 -0700

    Cleaning up

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,4 @@
-class Subscribe < ApplicationRecord
+class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :project
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root "static#home"
-  resources :users
+  resources :users do
+    resources :subscriptions, only: [:create, :destroy] do
+      resources :projects, only: [:index, :show]
+    end
+  end
 
   resources :projects do
     resources :updates
