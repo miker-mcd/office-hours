be8ecb57d6b352aacbe61992683aea2852c85dc3
Daniel Way <email": null>
2017-09-21 20:33:33 -0700

    add sms to project show page

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
-  belongs_to :staff, class_name: :User
-  belongs_to :student, class_name: :User
+  belongs_to :user
 
+  has_many :users
   has_many :updates
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,21 @@
-require 'json'
-require 'net/http'
 require 'twilio-ruby'
 
 
 class User < ApplicationRecord
-  include URI
-  include JSON
+
   devise :database_authenticatable, :registerable
          #:recoverable, :rememberable, :trackable, :validatable
   has_many :subscriptions
   has_many :projects
   has_many :updates, { through: :projects }
-  #+15416047255
 
-def text_user
+
+def text_user(tel,message)
   begin
     @client = Twilio::REST::Client.new ENV["ACCOUNT_SID"], ENV["AUTH_TOKEN"]
     message = @client.messages.create(
-      body: "Hello User!",
-      to: "+15416477999",
+      body: "#{message}",
+      to: "+1#{tel}",
       from: "+15416047255"
       )
   rescue Twilio::REST::TwilioError => e
